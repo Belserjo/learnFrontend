ca553c224307be86acab645b16d6aee1980bd5c9
Belserjo <belyatsky-sergey@.bk.ru>
2024-01-29 23:35:33 +0700

    Добавлены file и svg loader's, также в глобальные типы прописаны расширения для них, добавлен Ui-компонент - Button, и widget - themeSwitcher

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,7 +2,7 @@ import webpack from "webpack";
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import {BuildOptions} from "./types/config";
 
-export function buildLoaders({isDev}: BuildOptions):webpack.RuleSetRule[]{
+export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
     //Если не использовать тайпскрипт, то для работы с реакт нужен babel-loader
     const typeScriptLoader = {
         test: /\.tsx?$/,
@@ -10,30 +10,47 @@ export function buildLoaders({isDev}: BuildOptions):webpack.RuleSetRule[]{
         exclude: /node_modules/, //что исключаем для проверки
     }
 
-    const cssLoader =   {
-            test: /\.s[ac]ss$/i,
-            use: [
-                // Creates `style` nodes from JS strings
-                isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-                // Translates CSS into CommonJS
-                {
-                    loader: "css-loader",
-                    options: {
-                        modules: {
-                            auto: (resPath:string) => Boolean(resPath.includes('.module.scss')),
-                            localIdentName: isDev
-                                ? '[path][name]__[local]--[hash:base64:5]'
-                                : '[hash:base64:8]'},
+    const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+            // Translates CSS into CommonJS
+            {
+                loader: "css-loader",
+                options: {
+                    modules: {
+                        auto: (resPath: string) => Boolean(resPath.includes('.module.scss')),
+                        localIdentName: isDev
+                            ? '[path][name]__[local]--[hash:base64:5]'
+                            : '[hash:base64:8]'
+                    },
 
-                    }
-                },
-                // Compiles Sass to CSS
-                "sass-loader",
-            ],
-        };
-        return [
-            typeScriptLoader,
-            cssLoader,
+                }
+            },
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    };
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack'],
+    }
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader',
+            },
+        ],
+    };
+
+    return [
+        fileLoader,
+        svgLoader,
+        typeScriptLoader,
+        cssLoader,
     ]
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,13 +6,10 @@ import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme()
+    const {theme} = useTheme()
     return (
         <div className={classNames('app', {}, [theme])}>
-
             <Navbar/>
-            <button onClick={toggleTheme}>Сменить тему</button>
-
             <AppRouter/>
 
 
