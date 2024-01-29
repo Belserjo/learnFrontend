e6ef041ead1a5217b87a21e5f13aafa6531f2239
Belserjo <belyatsky-sergey@.bk.ru>
2024-01-29 21:58:27 +0700

    Разработан виджет навигационной панели, и первый ui-компонент, AppLink

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,18 @@
 import './styles/index.scss'
-import {Link, Route, Routes} from "react-router-dom";
 import {useTheme} from "app/providers/ThemeProvider";
-import {ClassNames} from "shared/lib/classNames/classNames";
+import {classNames} from "shared/lib/classNames/classNames";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 
 const App = () => {
     const {theme, toggleTheme} = useTheme()
     return (
-        <div className={ClassNames('app', {}, [theme])}>
+        <div className={classNames('app', {}, [theme])}>
+
+            <Navbar/>
             <button onClick={toggleTheme}>Сменить тему</button>
-            <Link to={'/'}>
-                <div>Main</div>
-            </Link>
-            <Link to={'/about'}>
-                <div>About Us</div>
-            </Link>
+
             <AppRouter/>
 
 
