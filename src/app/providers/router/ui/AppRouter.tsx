915e9a92ef5f0edcbe30c5cbc76c419c98aa260c
Belserjo <belyatsky-sergey@.bk.ru>
2024-01-30 22:05:37 +0700

    Добавлен widget-Sidebar, перенесены кнопки изменения темы в Sidebar

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,16 +1,24 @@
 import React, {Suspense} from 'react';
 import {Route, Routes} from "react-router-dom";
-import {routeConfig, routePath} from "shared/config/routeConfig/routeConfig";
+import {routeConfig} from "shared/config/routeConfig/routeConfig";
 
 const AppRouter = () => {
     return (
         <Suspense fallback={
-            <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+            <div style={{display: "flex", justifyContent: "center", flexGrow: "1", padding: "20px",}}>
                 Loading...
             </div>
         }><Routes>
             {Object.values(routeConfig).map(({element, path}) => (
-                <Route key={path} path={path} element={element}/>
+                <Route
+                    key={path}
+                    path={path}
+                    element={(
+                        <div className="page-wrapper">
+                            {element}
+                        </div>
+
+                    )}/>
             ))}
 
         </Routes>
