7feaf0e1df058ec6cd8ea89983fdc64fea9a8a7f
Belserjo <belyatsky-sergey@.bk.ru>
2024-02-03 16:55:04 +0700

    добавили jest, первый тест для classNames

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,37 @@
+import { classNames } from './classNames';
+
+describe('classNames', () => {
+    test('with only first param', () => {
+        expect(classNames('someCls')).toBe('someCls');
+    });
+    test('with additional class', () => {
+        const expected = 'someCls class1 class2';
+        expect(classNames('someCls', {}, ['class1', 'class2'])).toBe(expected);
+    });
+    test('with mods', () => {
+        const expected = 'someCls class1 class2 hovered scrollable';
+        expect(classNames(
+            'someCls',
+            { hovered: true, scrollable: true },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+    test('with mods false', () => {
+        const expected = 'someCls class1 class2 scrollable';
+        expect(classNames(
+            'someCls',
+            { hovered: false, scrollable: true },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    }); test('with mods undefined', () => {
+        const expected = 'someCls class1 class2';
+        expect(classNames(
+            'someCls',
+            { hovered: false, scrollable: undefined },
+            ['class1', 'class2'],
+        ))
+            .toBe(expected);
+    });
+});
